c6cb8e257375987e6fd5fe598aa50208bab458ea
Ubuntu <ubuntu@ip-172-31-27-127.us-east-2.compute.internal>
2020-03-26 21:00:50 +0000

    add age req

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,5 @@ class Cat < ApplicationRecord
     validates :name, presence: true
     validates :image, presence: true
     validates :interests, presence: true
+    validates :age, presence: true
 end
