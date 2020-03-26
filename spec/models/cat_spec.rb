b93ee391277f264097df701644b08eb65e165f7e
Ubuntu <ubuntu@ip-172-31-27-127.us-east-2.compute.internal>
2020-03-26 18:54:23 +0000

    fourth commit

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate name" do
+    cat = Cat.create
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  
+   it "doesn't create a cat without a name" do
+   cat_params = {
+     cat: {
+       age: 2,
+       enjoys: 'Walks in the park'
+     }
+   }
+   # Send the request to the  server
+   post '/cats', params: cat_params
+   # expect an error if the cat_params does not have a name
+   expect(response.status).to eq 422
+   # Convert the JSON response into a Ruby Hash
+   json = JSON.parse(response.body)
+   # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+   expect(json['name']).to include "can't be blank"
+ end
 end
--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
+    
+      it "creates a cat" do
+    # The params we are going to send with the request
+    cat_params = {
+      cat: {
+        name: 'Buster',
+        age: "4",
+        image: "Taco",
+        interests: 'Meow Mix, and plenty of sunshine.'
+      }
+    }
+
+    # Send the request to the server
+    post '/cats', params: cat_params
+
+    # Assure that we get a success back
+    expect(response).to have_http_status(:ok)
+
+    # Look up the cat we expect to be created in the Database
+    new_cat = Cat.last
+
+    # Assure that the created cat has the correct attributes
+    expect(new_cat.name).to eq('Buster')
+  end
 
 end
